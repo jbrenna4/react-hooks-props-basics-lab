d25c9bdbb781a31dbbd2176e587a74e56826e3f9
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-01 09:24:29 -0500

    almost done with final deliverable

---
+++ src/components/Links.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Links() {
+
+  const bio = props.bio
+
+  return (
+    <>
+      <h3>Links</h3>
+      <a></a>
+      <a></a>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    </>
+  );
+}
+
+export default Links;
